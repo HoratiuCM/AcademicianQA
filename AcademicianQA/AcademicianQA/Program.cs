34b5588d88da8258b46626bb734f02c9a872874b
Horatiu Morut <horatiu.morut@theaccesgroup.com>
2023-06-20 14:05:39 +0300

    commit1_branch1

--- AcademicianQA/AcademicianQA/Program.cs
+++ AcademicianQA/AcademicianQA/Program.cs
@@ -10,6 +10,10 @@ namespace AcademicianQA
     {
         static void Main(string[] args)
         {
+            int x=0;
+
+            Console.WriteLine(x);
+
             Console.WriteLine("Hello World");
 
             Console.ReadLine();
